92f4f98e421cb41cc33775d2ccbba25b959141b9
Oanh <hthoanh.95@gmail.com>
2022-12-29 10:43:34 +0700

    complete exercise 4.4

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,8 +2,13 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let sum = blogs.reduce((sum, item) => sum + item.likes, 0)
+    return sum
+}
 
 
 module.exports = {
-   dummy
+    dummy,
+    totalLikes
 }
